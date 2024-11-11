09f0e9d26f6a93c43c8aff0e874a5a34ec3747bb
devjiel <jeremy.leconardel@gmail.com>
2024-11-11 16:22:29 +0100

    Rework text size

--- frontend/lib/theme.dart
+++ frontend/lib/theme.dart
@@ -36,47 +36,36 @@ final kTheme = ThemeData(
 const kBaseTextStyle = TextStyle(
   color: kLightGreen,
   fontFamily: "Poppins",
-  height: 1.3,
 );
 
 final kTextTheme = TextTheme(
   titleLarge: kBaseTextStyle.copyWith(
-    fontSize: 32.0,
-    fontWeight: FontWeight.bold,
-    fontFamily: 'Poppins',
-  ),
-  titleMedium: kBaseTextStyle.copyWith(
-    fontSize: 18.0,
+    fontSize: 24.0,
     fontWeight: FontWeight.bold,
   ),
   titleSmall: kBaseTextStyle.copyWith(
-    fontSize: 18.0,
-    fontWeight: FontWeight.bold,
+    fontSize: 24.0,
+    color: kDarkGreen,
+    fontWeight: FontWeight.normal,
   ),
   bodyLarge: kBaseTextStyle.copyWith(
     fontSize: 16.0,
     fontWeight: FontWeight.bold,
   ),
-  bodyMedium: kBaseTextStyle.copyWith(
-    fontSize: 16.0,
-    fontWeight: FontWeight.normal,
-  ),
   bodySmall: kBaseTextStyle.copyWith(
-    fontSize: 14.0,
+    fontSize: 16.0,
+    color: kDarkGreen,
     fontWeight: FontWeight.normal,
   ),
-);
-
-const kTitleText = TextStyle(
-  color: kWhite,
-  fontSize: 38.0,
-  fontWeight: FontWeight.w900,
-);
-
-const kBodyText = TextStyle(
-  color: kWhite,
-  fontSize: 16.0,
-  fontWeight: FontWeight.normal,
+  labelLarge: kBaseTextStyle.copyWith(
+    fontSize: 12.0,
+    fontWeight: FontWeight.bold,
+  ),
+  labelSmall: kBaseTextStyle.copyWith(
+    fontSize: 10.0,
+    color: kDarkGreen,
+    fontWeight: FontWeight.normal,
+  ),
 );
 
 final kButtonStyle = ButtonStyle(
@@ -91,14 +80,9 @@ final kButtonStyle = ButtonStyle(
 
 extension BuildContextHelper on BuildContext {
   TextStyle? get titleLarge => Theme.of(this).textTheme.titleLarge;
-
   TextStyle? get titleMedium => Theme.of(this).textTheme.titleMedium;
-
   TextStyle? get titleSmall => Theme.of(this).textTheme.titleSmall;
-
   TextStyle? get bodyLarge => Theme.of(this).textTheme.bodyLarge;
-
   TextStyle? get bodyMedium => Theme.of(this).textTheme.bodyMedium;
-
   TextStyle? get bodySmall => Theme.of(this).textTheme.bodySmall;
 }
