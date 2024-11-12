681bf20ee16fa37cd1710a93945aff30efea40f7
devjiel <jeremy.leconardel@gmail.com>
2024-11-12 09:20:47 +0100

    Split file list component

--- frontend/lib/file_upload/ui/file_upload_section.dart
+++ frontend/lib/file_upload/ui/file_upload_section.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:greenshare/file_upload/ui/blocs/available_files_cubit.dart';
-import 'package:greenshare/file_upload/ui/widgets/file_widget.dart';
+import 'package:greenshare/file_upload/ui/widgets/file_list_widget.dart';
 import 'package:greenshare/l10n/localization.dart';
 import 'package:greenshare/theme.dart';
 import 'package:greenshare/ui/widgets/card.dart';
@@ -19,53 +17,7 @@ class FileUploadSection extends StatelessWidget {
           SizedBox(
             width: MediaQuery.of(context).size.width * 0.3,
             height: double.infinity,
-            child: GreenShareCard(
-              child: Padding(
-                padding: const EdgeInsets.all(kDefaultPadding),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      context.localization.availableDocuments,
-                      style: context.bodyLarge,
-                    ),
-                    const SizedBox(height: 12.0),
-                    Padding(
-                      padding: const EdgeInsets.all(kSmallPadding),
-                      child: SingleChildScrollView(
-                        child: BlocBuilder<AvailableFilesCubit, AvailableFilesState>(
-                          builder: (context, state) {
-                            // TODO split widget to tests differents states
-                            if (state is AvailableFilesLoading) {
-                              return const SizedBox(
-                                width: kDefaultPadding,
-                                height: kDefaultPadding,
-                                child: CircularProgressIndicator(),
-                              );
-                            } else if (state is AvailableFilesError) {
-                              return Text(
-                                'Error: ${state.message}',
-                                style: context.bodySmall?.copyWith(color: kRed),
-                              );
-                            } else if (state is AvailableFilesLoaded) {
-                              if (state.files.isEmpty) {
-                                return Text(context.localization.noAvailableDocuments, style: context.bodySmall);
-                              } else {
-                                return Column(
-                                  children: state.files.map((file) => FileWidget(file: file)).toList(),
-                                );
-                              }
-                            } else {
-                              return Container();
-                            }
-                          },
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
+            child: const FileListWidget(),
           ),
           const SizedBox(width: kMaxPadding),
           Expanded(
---
+++ frontend/lib/file_upload/ui/widgets/file_list_widget.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:greenshare/file_upload/ui/blocs/available_files_cubit.dart';
+import 'package:greenshare/file_upload/ui/widgets/file_widget.dart';
+import 'package:greenshare/l10n/localization.dart';
+import 'package:greenshare/theme.dart';
+import 'package:greenshare/ui/widgets/card.dart';
+
+class FileListWidget extends StatelessWidget {
+  const FileListWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GreenShareCard(
+      child: Padding(
+        padding: const EdgeInsets.all(kDefaultPadding),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              context.localization.availableDocuments,
+              style: context.bodyLarge,
+            ),
+            const SizedBox(height: 12.0),
+            Padding(
+              padding: const EdgeInsets.all(kSmallPadding),
+              child: SingleChildScrollView(
+                child: BlocBuilder<AvailableFilesCubit, AvailableFilesState>(
+                  builder: (context, state) {
+                    if (state is AvailableFilesLoading) {
+                      return const SizedBox(
+                        width: kDefaultPadding,
+                        height: kDefaultPadding,
+                        child: CircularProgressIndicator(),
+                      );
+                    } else if (state is AvailableFilesError) {
+                      return Text(
+                        'Error: ${state.message}',
+                        style: context.bodySmall?.copyWith(color: kRed),
+                      );
+                    } else if (state is AvailableFilesLoaded) {
+                      if (state.files.isEmpty) {
+                        return Text(context.localization.noAvailableDocuments, style: context.bodySmall);
+                      } else {
+                        return Column(
+                          children: state.files.map((file) => FileWidget(file: file)).toList(),
+                        );
+                      }
+                    } else {
+                      return Container();
+                    }
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
