c70a5850063eaf8f43693547766aa28a422f6e1b
devjiel <jeremy.leconardel@gmail.com>
2024-11-12 21:41:31 +0100

    [WIP] add Drop zone

--- frontend/lib/file_upload/ui/file_upload_section.dart
+++ frontend/lib/file_upload/ui/file_upload_section.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dropzone/flutter_dropzone.dart';
 import 'package:greenshare/common/ui/widgets/card.dart';
 import 'package:greenshare/file_upload/ui/widgets/file_list_widget.dart';
 import 'package:greenshare/l10n/localization.dart';
@@ -24,42 +25,53 @@ class FileUploadSection extends StatelessWidget {
           ),
           const SizedBox(width: kMaxPadding),
           Expanded(
-            child: GreenShareCard(
-              dottedBorder: true,
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    IconButton(
-                      icon: const Icon(Icons.cloud_upload),
-                      iconSize: 48,
-                      onPressed: () async {
-                        // TODO https://github.com/miguelpruivo/flutter_file_picker/wiki/Setup
-                        // TODO https://pub.dev/packages/file_picker
-                        final result = await FilePicker.platform.pickFiles(type: FileType.any, allowMultiple: false);
-
-                        if (result != null && result.files.isNotEmpty) {
-                          final fileBytes = result.files.first.bytes;
-                          final fileName = result.files.first.name;
-
-                          print('File name: $fileName');
-
-                          // upload file
-                          //await FirebaseStorage.instance.ref('uploads/$fileName').putData(fileBytes);
-                        }
-                      },
-                    ),
-                    Text(
-                      context.localization.importFile,
-                      style: context.bodyLarge,
-                    ),
-                    Text(
-                      context.localization.fileMaxSize(50),
-                      style: context.bodySmall,
-                    ),
-                  ],
+            child: Stack(
+              children: [
+                // TODO: https://pub.dev/packages/flutter_dropzone
+                DropzoneView(
+                  operation: DragOperation.copy,
+                  onDropFile: (file) {
+                    print('Received file: ${file.name}');
+                  },
                 ),
-              ),
+                GreenShareCard(
+                  dottedBorder: true,
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        IconButton(
+                          icon: const Icon(Icons.cloud_upload),
+                          iconSize: 48,
+                          onPressed: () async {
+                            // TODO https://github.com/miguelpruivo/flutter_file_picker/wiki/Setup
+                            // TODO https://pub.dev/packages/file_picker
+                            final result = await FilePicker.platform.pickFiles(type: FileType.any, allowMultiple: false);
+
+                            if (result != null && result.files.isNotEmpty) {
+                              final fileBytes = result.files.first.bytes;
+                              final fileName = result.files.first.name;
+
+                              print('File name: $fileName');
+
+                              // upload file
+                              //await FirebaseStorage.instance.ref('uploads/$fileName').putData(fileBytes);
+                            }
+                          },
+                        ),
+                        Text(
+                          context.localization.importFile,
+                          style: context.bodyLarge,
+                        ),
+                        Text(
+                          context.localization.fileMaxSize(50),
+                          style: context.bodySmall,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
